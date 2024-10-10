5cb35732ca3d04c743ecd606cbe14e5df3a3bbb2
TMajlu <thomasmajnie@gmail.com>
2024-10-10 14:27:34 +0200

    Thomas Nielsen

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -5,6 +5,16 @@
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
 
+function timerStatus(remaining){
+  if (remaining===0){
+    return "Phil's cake is ready!"
+  }
+  else if (remaining>0){
+    return "The cake is still baking!"
+  }
+  return "You didn't set a timer!"
+}
+
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +23,14 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+
+function estimatePrepTime(ingredients, prepTime){
+  if(prepTime>0){
+    return ingredients.length * prepTime
+  }
+  return ingredients.length * 2
+
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -30,6 +48,20 @@
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
 
+function calculateQuantities(ingredients, layers){
+  const quantities={sugar:0, eggs:0}
+  for (let i=0; i<ingredients.length; i++){
+    if(ingredients[i]=='sugar'){
+      quantities.sugar=layers*100
+    }
+    else if(ingredients[i]=='eggs'){
+      quantities.eggs=layers*2
+    }
+  }
+  return quantities
+}
+
+
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +74,14 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+
+function improveRecipe(ingredients, portions){
+  for (let key in ingredients){
+    ingredients[key]*=portions
+  }
+
+  return ingredients
+}
 
 // Don't change the code below this line
 module.exports = {
